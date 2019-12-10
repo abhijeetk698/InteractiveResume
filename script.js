896b71574b7c8a4851b95421a9832fa0997b7019
Abhijeet Jha <35653985+abhijeetk698@users.noreply.github.com>
2019-12-10 18:13:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+// As I am working on it I have yet to add a lot of features on it involving backend Support 
+// Github Api to show my activity I have yet to add some features like compatiblity with mobile
+// devices and phones
